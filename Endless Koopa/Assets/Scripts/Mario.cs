d9832db5b289048987bf877cad84117fc3b04475
José Luis Maquieira <jmaquieira@basecodeit.com>
2019-07-04 19:42:07 -0300

    adds mario and pool object

---
+++ Endless Koopa/Assets/Scripts/Mario.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Mario : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    private Animator animator;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Walk(float direction)
+    {
+        if (direction != 0)
+        {
+            animator.SetTrigger("marioWalk");
+        }
+        else
+        {
+            animator.SetTrigger("marioIdle");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Walk(Input.GetAxis("Horizontal"));
+    }
+}
---
+++ Endless Koopa/Assets/Scripts/PoolObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PoolObject : MonoBehaviour
+{
+    public float minYPosition = -2f;
+    public float maxYPosition = 2f;
+    public float minXPosition = 10f;
+    public float maxXPosition = 10f;
+}
